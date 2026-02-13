dd9d6b81d82f09bb49f6e5a69e0850771a8c9b53
RenoMO-lab <renaud.molinier@outlook.com>
2026-02-13 15:20:30 +0800

    Default theme to dark for first-time users

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider
     attribute="class"
-    defaultTheme="system"
+    defaultTheme="dark"
     enableSystem
     storageKey="cra-theme"
     disableTransitionOnChange
